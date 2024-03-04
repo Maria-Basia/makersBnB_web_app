ff4346f6be746cbac1bf7a7ba4549f6b4ec4f5b8
kevin eboda <kevineboda@gmail.com>
2024-03-04 17:07:48 +0000

    homepage.html added

--- lib/user.py
+++ lib/user.py
@@ -9,3 +9,17 @@ class User:
         return self.__dict__ == other.__dict__
     def __repr__(self):
         return f"User({self.id}, {self.email_address}, {self.password})"
+    
+    def is_valid(self):
+        return self.valid_length(self.password) and self.has_special_characters(self.password)
+
+    def valid_length(self):
+        return len(self.password) > 7
+
+    def has_special_characters(self):
+        characters = ["!","@","$","%","&"]
+        for char in characters:
+            if char in self.password:
+                return True
+        return False
+        
