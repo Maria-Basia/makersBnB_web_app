77e66859cf6d77febecad7200ac3f52bbda28659
Benjamin Pearl <ben.pearl@btinternet.com>
2024-03-05 12:19:53 +0000

    test merge

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ app = Flask(__name__)
 # Returns the homepage
 #testing if this works
 #   ; open http://localhost:5000/index
-
+@app.route('/index', methods=['GET'])
+def get_index():
+    return render_template('index.html')
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
@@ -30,6 +32,3 @@ if __name__ == '__main__':
 
 
 
-@app.route('/inde', methods=['GET'])
-def get_x():
-    return render_template('index.html')
